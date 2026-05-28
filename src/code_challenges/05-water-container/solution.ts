4ebb4240a4158dc76d2f0dcf4835d029b0b57cc1
Sebastian Castañeda <castanedacs@gmail.com>
2026-05-28 13:25:04 +0200

    Missing changes

--- src/code_challenges/05-water-container/solution.ts
+++ src/code_challenges/05-water-container/solution.ts
@@ -1,12 +1,9 @@
 export function maxArea(height: number[]): number {
-  if (height.length === 0) return 0;
   let maxArea = 0;
   let left = 0;
   let right = height.length - 1;
   while (left < right) {
-    const width = right - left;
-    const currentHeight = Math.min(height[left], height[right]);
-    maxArea = Math.max(maxArea, width * currentHeight);
+    maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
     if (height[left] <= height[right]) {
       left++;
     } else {
